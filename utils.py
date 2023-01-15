47a340188ff9b2b8755f4b79c65c8db51503bf84
Wolkensteine <bokaj.bb22@gmail.com>
2023-01-15 16:29:53 +0100

    compressing
    Removing unused imports
    Adding compression function
    correcting things

--- utils.py
+++ utils.py
@@ -1,12 +1,11 @@
-import PIL
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 
 
-def compress_image(image_path, safe_location):
-    img = Image.open(image_path)
+def save_compressed_image(img, safe_location, image_codec="JPEG", save_quality=85):
+    img.save(safe_location, image_codec, optimize=True, quality=save_quality)
 
 
-def watermark_image(image_path, image = None, watermark_text = "Watermark", watermark_location = (5, 5), watermark_image_path = None, watermark_image_location = (30, 5), watermark_image_size = (50, 50), safe_location = None, return_image = True):
+def watermark_image(image_path, image = None, watermark_text = "Watermark", watermark_location = (5, 5), watermark_image_path = None, watermark_image_location = (30, 5), watermark_image_size = (50, 50), safe_location = None, return_image = True, save_compressed = False, save_image_codec = "JPEG", save_quality_level = 5):
     # TODO check, if every value is provided in the right format so it can be used. Else return an error message
 
     # either use provided image or find image via provided path
@@ -24,7 +23,10 @@ def watermark_image(image_path, image = None, watermark_text = "Watermark", wate
     # Safe or return the image dependent on whether a location for saving is provided or not
     if safe_location is not None:
         # safe to path
-        print()
+        if save_compressed:
+            save_compressed_image(img, safe_location, save_image_codec, save_quality_level)
+        else:
+            img.save(safe_location)
         # check if image should also be returned in addition to be saved
         if return_image:
             return watermarked
