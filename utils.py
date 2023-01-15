60ca92d81801db641b6ec795f5ca9167c9d513d0
Wolkensteine <bokaj.bb22@gmail.com>
2023-01-15 16:15:38 +0100

    Correcting things

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def compress_image(image_path, safe_location):
     img = Image.open(image_path)
 
 
-def watermark_image(image_path, image = None, watermark_text = "Watermark", watermark_location = (5, 5), watermark_image_path = None, watermark_image_location = (5, 30), watermark_image_size = (50, 50), safe_location = None, return_image = True):
+def watermark_image(image_path, image = None, watermark_text = "Watermark", watermark_location = (5, 5), watermark_image_path = None, watermark_image_location = (30, 5), watermark_image_size = (50, 50), safe_location = None, return_image = True):
     # TODO check, if every value is provided in the right format so it can be used. Else return an error message
 
     # either use provided image or find image via provided path
@@ -18,7 +18,8 @@ def watermark_image(image_path, image = None, watermark_text = "Watermark", wate
     # Image will be modified
     watermarked = ImageDraw.Draw(img)
     watermarked.text(watermark_location, watermark_text)
-    watermarked.bitmap()
+    if watermark_image_path is not None:
+        watermarked.bitmap(watermark_image_location, Image.open(watermark_image_path).resize(watermark_image_size))
 
     # Safe or return the image dependent on whether a location for saving is provided or not
     if safe_location is not None:
